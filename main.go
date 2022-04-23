2477b5455df3c1746d0888acb9a86d82b8a0242a
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2022-04-23 23:04:12 +0200

    [WIP] interfaces

--- main.go
+++ main.go
@@ -11,37 +11,68 @@ import (
 	"time"
 )
 
+type GenericRecord interface{
+	Parse(string) error
+}
+
+// Record is the common interface between all types of records (CODA lines)
+type Record struct{}
+
+// Parse is the generic record string parser
+func (r *Record) Parse(s string) error {
+	t := reflect.TypeOf(r).Elem()
+	v := reflect.ValueOf(r).Elem()
+	fmt.Printf("Type %v value %v\n", t, v)
+	for i := 0; i < t.NumField(); i++ {
+		tt := t.Field(i)
+		vv := v.Field(i)
+		fmt.Printf("Field %v: name %v, type %v tag %v value %v\n", i, tt.Name, tt.Type, tt.Tag, vv)
+		offset, err := strconv.Atoi(tt.Tag.Get("offset"))
+		if err != nil {
+			return err
+		}
+		length, err := strconv.Atoi(tt.Tag.Get("length"))
+		if err != nil {
+			return err
+		}
+		switch vv.Type() {
+		case reflect.TypeOf(int(0)):
+			value, err := strconv.Atoi(s[offset : offset+length])
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Value is %d\n", value)
+			vv.SetInt(int64(value))
+		case reflect.TypeOf(string("")):
+			value := strings.TrimSpace(s[offset : offset+length])
+			fmt.Printf("Value is %s\n", value)
+			vv.SetString(value)
+		case reflect.TypeOf(bool(false)):
+			value := s[offset:offset+length] != ""
+			fmt.Printf("Value is %t\n", value)
+			vv.SetBool(value)
+		case reflect.TypeOf(time.Time{}):
+			value, err := time.Parse("020106", s[offset:offset+length])
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Value is %v\n", value)
+			vv.Set(reflect.ValueOf(value))
+		}
+	}
+	return nil
+}
+
 type InitialRecord struct {
+	*Record
 	CreationDate             time.Time `offset:"5" length:"6"`
 	BankIdentificationNumber int       `offset:"11" length:"3"`
 	IsDuplicate              bool      `offset:"16" length:"1"`
 	Reference                string    `offset:"24" length:"10"`
 }
 
-var sample = `0000002011830005        59501140  ACCOUNTANCY J DE KNIJF    BBRUBEBB   00412694022 00000                                       2`
-
-//for _, field := range reflect.VisibleFields(reflect.TypeOf(*transactionPurposeRecord)) {
-//	if field.Type.Kind() == reflect.Int {
-//		fmt.Printf("Found an int %s!\n", field.Name)
-//	}
-//	if field.Type.Kind() == reflect.ValueOf(time.Time{}).Kind() {
-//		fmt.Printf("Found time %s!\n", field.Name)
-//	}
-//	positions := make(map[string]int)
-//	tag := field.Tag.Get("coda")
-//	if tag != "" {
-//		fmt.Printf("%s: %s\n", field.Type, tag)
-//		for _, descriptions := range strings.Split(tag, ",") {
-//			s := strings.Split(descriptions, ":")
-//			o, _ := strconv.Atoi(string(s[1]))
-//			positions[s[0]] = o
-//		}
-//		fmt.Printf("%v\n", positions)
-//	}
-//	if field.Type.Kind() == reflect.Int {
-//		fmt.Printf("Found an int %s!\n", field.Name)
-//		field.SetInt(parseInt(sample[positions["offset"] : positions["offset"]+positions["length"]]))
-//	}
+//func (r *InitialRecord) Parse(s string) error {
+//	return r.Parse(s)
 //}
 
 func parseDecimal(s string) (decimal.Decimal, error) {
@@ -68,62 +99,16 @@ func printFields(x interface{}) {
 	}
 }
 
+var sample = `0000002011830005        59501140  ACCOUNTANCY J DE KNIJF    BBRUBEBB   00412694022 00000                                       2`
+
 func main() {
 	// r := InitialRecord{time.Now(), 123, false, "456"}
-	r := InitialRecord{}
-	// Get dynamic type and value, dereference pointer
-	t := reflect.TypeOf(&r).Elem()
-	v := reflect.ValueOf(&r).Elem()
-	for i := 0; i < t.NumField(); i++ {
-		tt := t.Field(i)
-		vv := v.Field(i)
-		offset, _ := strconv.Atoi(tt.Tag.Get("offset"))
-		length, _ := strconv.Atoi(tt.Tag.Get("length"))
-		s := sample[offset : offset+length]
-		switch vv.Type() {
-		case reflect.TypeOf(int(0)):
-			value, err := strconv.Atoi(s)
-			if err != nil {
-				log.Fatalf("Error setting value\n")
-			}
-			fmt.Printf("Value is %d\n", value)
-			vv.SetInt(int64(value))
-		case reflect.TypeOf(string("")):
-			value := strings.TrimSpace(s)
-			fmt.Printf("Value is %s\n", value)
-			vv.SetString(value)
-		case reflect.TypeOf(bool(false)):
-			value := s != ""
-			fmt.Printf("Value is %t\n", value)
-			vv.SetBool(value)
-		case reflect.TypeOf(time.Time{}):
-			value, err := time.Parse("020106", s)
-			if err != nil {
-				log.Fatalf("Error setting value\n")
-			}
-			fmt.Printf("Value is %v\n", value)
-			vv.Set(reflect.ValueOf(value))
-		}
+	r := &InitialRecord{}
+	err := r.Parse(sample)
+	if err != nil {
+		log.Fatalf("Could not parse sample %v: %v\n", r, err)
+		return
 	}
-	//t := reflect.TypeOf(*r)
-	//v := reflect.ValueOf(*r)
-	//for _, field := range reflect.VisibleFields(t) {
-	//	vv := v.FieldByIndex(field.Index)
-	//	offset, _ := strconv.Atoi(field.Tag.Get("offset"))
-	//	length, _ := strconv.Atoi(field.Tag.Get("length"))
-	//	fmt.Printf("Field %s type %v value %v offset %d length %d\n", field.Name, field.Type, vv, offset, length)
-	//	switch field.Type {
-	//	case reflect.TypeOf(int(0)):
-	//		value, err := strconv.Atoi(sample[offset : offset+length])
-	//		if err != nil {
-	//			log.Fatalf("Error setting value\n")
-	//		}
-	//		fmt.Printf("Value is %d\n", value)
-	//		vv.SetInt(int64(value))
-	//	case reflect.TypeOf(time.Time{}):
-	//		fmt.Printf("FOUND A TIMESTAMP\n")
-	//	}
-	//}
 
 	// Pretty print
 	pprint, err := json.MarshalIndent(r, "", "    ")
